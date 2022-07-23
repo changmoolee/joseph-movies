fbdebd3d424d7baf779f6f43c8174b3fb711f1a0
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-07-23 19:06:46 +0900

    Refactor: Tabs ui 내부에 컴포넌트를 따로 분리하고 data fetching도 분리

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
-import axios from "axios";
+import React from "react";
 import { Tabs } from "joseph-ui-kit";
 import {
   Tab,
@@ -9,47 +7,14 @@ import {
   TabsDescriptions,
 } from "joseph-ui-kit/dist/components/Tabs/Tabs";
 import Header from "../components/Header";
-import Contents from "../components/Contents";
 import Footer from "../components/Footer";
 import SearchBox from "../components/SearchBox";
-import SkeletonContents from "../components/SkeletonContents";
+import Popular from "../components/Popular";
+import NowPlaying from "../components/NowPlaying";
+import UpComing from "../components/UpComing";
+import TopRated from "../components/TopRated";
 
 const Main = () => {
-  const [loading, setLoading] = useState(false);
-  const [popular, setPopular] = useState({});
-  const [nowPlaying, setNowPlaying] = useState({});
-  const [upComing, setUpComing] = useState({});
-  const [topRated, setTopRated] = useState({});
-
-  useEffect(() => {
-    setLoading(true);
-    axios
-      .all([
-        axios.get(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=ko&page=1`
-        ),
-        axios.get(
-          `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}&language=ko&page=1`
-        ),
-        axios.get(
-          `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_API_KEY}&language=ko&page=1`
-        ),
-        axios.get(
-          `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_API_KEY}&language=ko&page=1`
-        ),
-      ])
-      .then(
-        axios.spread((res1, res2, res3, res4) => {
-          setPopular(res1.data);
-          setNowPlaying(res2.data);
-          setUpComing(res3.data);
-          setTopRated(res4.data);
-        })
-      )
-      .then(() => setLoading(false))
-      .catch((err) => console.log(err));
-  }, []);
-
   return (
     <>
       <Header />
@@ -64,16 +29,19 @@ const Main = () => {
           </TabList>
           <TabsDescriptions>
             <TabsDescription>
-              {loading ? <SkeletonContents /> : <Contents data={popular} />}
+              <Popular />
             </TabsDescription>
+
             <TabsDescription>
-              {loading ? <SkeletonContents /> : <Contents data={nowPlaying} />}
+              <NowPlaying />
             </TabsDescription>
+
             <TabsDescription>
-              {loading ? <SkeletonContents /> : <Contents data={upComing} />}
+              <UpComing />
             </TabsDescription>
+
             <TabsDescription>
-              {loading ? <SkeletonContents /> : <Contents data={topRated} />}
+              <TopRated />
             </TabsDescription>
           </TabsDescriptions>
         </>
