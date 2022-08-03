e3253ae85c847f6e292fd6991ea661cc85c1afd1
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-08-03 09:36:26 +0900

    Feat: SortAccordion 컴포넌트의 영화 정렬 기능(DropDown) 각 Panel들에서 활용가능하도록 적용

--- src/components/NowPlayingTopPanel.tsx
+++ src/components/NowPlayingTopPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import axios from "axios";
 import { Button } from "joseph-ui-kit";
@@ -10,13 +10,23 @@ const NowPlayingTopPanel = () => {
   const [loading, setLoading] = useState(true);
   const [nowPlaying, setNowPlaying] = useState([]);
   const [page, setPage] = useState(1);
+
+  const [sortedMovies, setSortedMovies] = useState([]);
+  const [sortedPage, setSortedPage] = useState(1);
+
   const [selected, setSelected] = useState<{
     id: number | string;
     value: number | string;
   }>({ id: "popularity.desc", value: "인기도 내림차순" });
 
+  const didMount = useRef(false);
+
   const morePage = () => {
     setPage((page) => page + 1);
+  };
+
+  const moreSortedPage = () => {
+    setSortedPage((page) => page + 1);
   };
 
   useEffect(() => {
@@ -32,18 +42,66 @@ const NowPlayingTopPanel = () => {
       .catch((err) => console.log(err));
   }, [page]);
 
+  useEffect(() => {
+    if (didMount.current) {
+      setLoading(true);
+      setSortedMovies([]);
+      setSortedPage(1);
+      axios
+        .get(
+          `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=ko&page=1&sort_by=${selected.id}`
+        )
+        .then((res) => {
+          setSortedMovies((popular) => popular.concat(res.data.results));
+        })
+        .then(() => {
+          setLoading(false);
+        })
+        .catch((err) => console.log(err));
+    } else {
+      didMount.current = true;
+    }
+  }, [selected]);
+
+  useEffect(() => {
+    if (sortedPage > 1) {
+      setLoading(true);
+      axios
+        .get(
+          `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=ko&page=${sortedPage}&sort_by=${selected.id}`
+        )
+        .then((res) => {
+          setSortedMovies((popular) => popular.concat(res.data.results));
+        })
+        .then(() => {
+          setLoading(false);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [sortedPage]);
+
   return loading ? (
     <>
       <SortAccordion />
       <SkeletonContents />
     </>
-  ) : (
+  ) : sortedMovies.length === 0 ? (
     <>
       <SortAccordion setSelected={setSelected} />
       <Contents data={nowPlaying} />
       {nowPlaying.length >= 20 * page ? (
         <ButtonContainer>
           <Button name="더보기" padding="10px 70px" onClick={morePage} />
+        </ButtonContainer>
+      ) : null}
+    </>
+  ) : (
+    <>
+      <SortAccordion setSelected={setSelected} />
+      <Contents data={sortedMovies} />
+      {sortedMovies.length >= 20 * sortedPage ? (
+        <ButtonContainer>
+          <Button name="더보기" padding="10px 70px" onClick={moreSortedPage} />
         </ButtonContainer>
       ) : null}
     </>
